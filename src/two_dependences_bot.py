fd0522c7a5253b1b44ebe1ae3ec563ebf392a0da
Piclog <lmtuyen1165@gmail.com>
2025-12-24 00:18:13 +0700

    fix:delete fails

--- src/two_dependences_bot.py
+++ src/two_dependences_bot.py
@@ -2,22 +2,29 @@ import keyboard
 import time
 import mss
 
-note_highway = {"top": 770, "left": 950, "width": 1, "height": 1}
-spool = {"top": 896, "left": 1858, "width": 1, "height": 1}
+note_highway = {"top": 836, "left": 950, "width": 1, "height": 1}
+spool = {"top": 905, "left": 1850, "width": 1, "height": 1}
 
 def get_note_highway_pixel_color():
-    img=mss.mss().grab(note_highway)
-    b,g,r,a=img.raw
-    return (r,g,b)
+    try:
+        img = mss.mss().grab(note_highway)
+        b, g, r, a = img.raw
+        return (r, g, b)
+    except Exception as e:
+        return None
 
 def get_spool_pixel_color():
-    img=mss.mss().grab(spool)
-    b,g,r,a=img.raw
-    return (r,g,b)
+    try:
+        img = mss.mss().grab(spool)
+        b, g, r, a = img.raw
+        return (r, g, b)
+    except Exception as e:
+        return None
 
 while True:
     note_highway_color = get_note_highway_pixel_color()
-    spoolcolor = get_spool_pixel_color()
+    spool_color = get_spool_pixel_color()
 
-    if note_highway_color == (0, 255, 0) and spoolcolor == (51, 51, 51):
+    if note_highway_color == (0, 255, 0) and spool_color == (51, 51, 51):
         keyboard.press_and_release('space')
+        time.sleep(0.14)
